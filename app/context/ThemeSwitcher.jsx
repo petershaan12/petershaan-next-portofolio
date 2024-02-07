3b89aa3997c3cabe0f9dfc3de7fbe634abd996d0
petershaan12 <peterhiku12@gmail.com>
2024-02-07 13:40:52 +0700

    mengganti mode darkmodenya menggunakan waktu bukan sistem

--- app/context/ThemeSwitcher.jsx
+++ app/context/ThemeSwitcher.jsx
@@ -3,21 +3,37 @@
 import { useTheme } from "next-themes";
 import { SunIcon } from "@heroicons/react/24/outline";
 import { TbMoonStars } from "react-icons/tb";
+import { useEffect } from "react";
 
 const ThemeSwitcher = () => {
-  const { systemTheme, theme, setTheme } = useTheme();
+  const { theme, setTheme } = useTheme();
+
+  const isNightTime = () => {
+    const currentHour = new Date().getHours();
+    console.log(currentHour);
+    return currentHour >= 18 || currentHour < 6; 
+  };
+
+  useEffect(() => {
+    // Cek waktunya apakah pagi atau petang
+    if (isNightTime()) {
+      setTheme("dark");
+    } else {
+      setTheme("light");
+    }
+  }, []); 
 
   const renderThemeChanger = () => {
-    const currentTheme = theme === "system" ? systemTheme : theme;
-
-    return currentTheme === "dark" ? 
-    <SunIcon
-        className="w-6 h-6 text-yellow-500"
-        role="button"
-        onClick={() => setTheme("light")}
-    /> 
-    : 
-    <TbMoonStars onClick={() => setTheme("dark")} />;
+    return theme === "dark" ? (
+      <TbMoonStars className="w-4 text-white ml-4" onClick={() => setTheme("light")} />
+    ) : (
+      
+      <SunIcon
+      className="w-6 h-6 text-yellow-500"
+      role="button"
+      onClick={() => setTheme("dark")}
+    />
+    );
   };
 
   return <>{renderThemeChanger()}</>;
